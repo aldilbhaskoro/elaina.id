f5e11db1c76a9476eaaea3b32b3e63b3a64aa2ef
aldilbhaskoro <85004026+aldilbhaskoro@users.noreply.github.com>
2025-09-08 16:07:37 +0700

    update

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export', // ⬅️ ini pengganti `next export`
+  output: 'export',        // pakai static export
+  images: {
+    unoptimized: true,     // kalau pakai <Image>
+  },
+  trailingSlash: true,     // supaya route /about → /about/index.html
 }
 
 module.exports = nextConfig
